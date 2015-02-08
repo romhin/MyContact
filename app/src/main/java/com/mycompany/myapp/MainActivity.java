3a3173fd5d1cb0301787bb536eae0ef182128d44
rom <romhin25@gmail.com>
2015-02-09 01:01:33 +0400

    tree

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.mycompany.myapp;
 
-import android.app.*;
-import android.os.*;
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity implements
+    LoaderManager.LoaderCallbacks<Cursor> {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
